23a783adba24ee6a6f8fc5f985c2b63be6131622
annts095 <anantasuku@team-lab.com>
2023-02-04 20:48:43 +0900

    feat: 更新ボタンを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,19 @@
 <script lang="ts" setup>
 import { CatResponse } from '~/types'
 
-const { pending, error, data } = useFetch<CatResponse[]>(
+const { pending, error, data, refresh } = useFetch<CatResponse[]>(
   'https://api.thecatapi.com/v1/images/search?limit=10'
 )
 </script>
 
 <template>
-  <div v-if="!pending && data" class="content">
-    <cat-card-list :cat-list="data" />
+  <div>
+    <button class="refresh-button" @click="refresh">
+      更新
+    </button>
+    <div v-if="!pending && data" class="content">
+      <cat-card-list :cat-list="data" />
+    </div>
   </div>
 </template>
 
