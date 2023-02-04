78480a5c68c7747611bdc10618676f36efee2501
annts095 <anantasuku@team-lab.com>
2023-02-04 20:34:19 +0900

    feat: トップページ作成

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { CatResponse } from '~/types'
+
+const { pending, error, data } = useFetch<CatResponse[]>(
+  'https://api.thecatapi.com/v1/images/search?limit=10'
+)
+</script>
+
+<template>
+  <div v-if="!pending && data" class="content">
+    <cat-card-list :cat-list="data" />
+  </div>
+</template>
+
+<style scoped>
+.content {
+  margin: auto;
+}
+</style>
